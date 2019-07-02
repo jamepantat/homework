05da2f14cb253c6d88cb1125c8bf407c1287a952
Pantat Srikongkaew <52206979+jamepantat@users.noreply.github.com>
2019-07-02 21:18:49 +0700

    Add files via upload

---
+++ homework01.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{	cout<<"Student code : 6206021610129" << endl;
+	cout<<"Student name : Pantat Srikongkaew" << endl;
+	cout<<"     Division: Infomation Techology" << endl;
+	cout<<"        Year : 1 "   << endl;
+	cout<<"        Room : RA"   << endl;
+	cout<<"         Sex : Male"  << endl;
+	cout<<"         Age : 18"   << endl;
+	 
+	cout<<"    #    " << endl;
+	cout<<"   # #   " << endl;
+	cout<<"  #   #  " << endl;
+	cout<<" #     # " << endl;
+	cout<<" ####### " << endl;
+	cout<<" #     # " << endl;
+	return(0);
+}
